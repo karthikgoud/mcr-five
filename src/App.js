e907f0d6afa9d74eb9176bb7aced96c11efcf1f2
karthikgoud <karthikrv09@gmail.com>
2023-07-01 21:52:35 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { NavLink, Route, Routes } from "react-router-dom";
 import "./App.css";
+import RecipeDetails from "./pages/RecipeDetails/RecipeDetails";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">app</header>
+      <header className="App-header">
+        <nav>
+          <NavLink to="/home">Home</NavLink>
+        </nav>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/detail/:recipeId" element={<RecipeDetails />} />
+        </Routes>
+      </header>
     </div>
   );
 }
